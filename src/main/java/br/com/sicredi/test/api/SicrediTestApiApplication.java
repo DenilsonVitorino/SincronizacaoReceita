b7a27a9f5e046d1564bfc881ba0709cfeea7dddb
Denilson Vitorino <dv22pro@gmail.com>
2020-10-25 21:52:23 -0300

    import project

---
+++ src/main/java/br/com/sicredi/test/api/SicrediTestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.test.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediTestApiApplication.class, args);
+	}
+
+}
